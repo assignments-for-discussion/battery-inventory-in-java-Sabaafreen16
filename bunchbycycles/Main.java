81476f0d959c9b9d737f80801cc28f0dbfe559ce
Sabaafreen16 <102548648+Sabaafreen16@users.noreply.github.com>
2022-11-14 17:58:44 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -7,8 +7,24 @@ public class Main {
     public int highCount = 0;
   };
 
-  static CountsByUsage countBatteriesByUsage(int[] cycles) {
-    CountsByUsage counts = new CountsByUsage();
+  static CountsByUsage countBatteriesByUsage(int[] cycles) 
+  {
+     CountsByUsage counts = new CountsByUsage();
+    for(int i=0;i<cycles.length;i++)
+    {
+    if cycles[i]<410 // to check if the charge cycle is less than 410
+    {
+    counts.lowcount++; // incrementing low count if charge is less than 420
+    }
+      else if cycles[i] > 410 && cycles[i] <950  // to check if the charge cycle is greater than 410 and less than 950
+      {
+      counts.mediumcount++;// incrementing medium count if charge is greater than 410 and less than 950
+      }
+      else cycles[i]>=950  // to check if the charge cycle is greater than or equal to 950
+      {
+        counts.highcount++; // incrementing high count if the charge cycle is greater than or equal to 950
+      }
+    }
     return counts;
   }
 
